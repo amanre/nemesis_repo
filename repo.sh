31a73d33332467c0b157d94d7c1a7ffdb499c851
amanre <amanre556@gmail.com>
2024-04-28 03:39:23 +0200

    update

---
+++ repo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd x86_64
+rm nemesis_repo*
+
+echo "repo-add"
+repo-add -n -R -v  nemesis_repo.db.tar.gz *.pkg.tar.zst
+rm -v nemesis_repo.db
+rm -v nemesis_repo.files
+mv -v nemesis_repo.db.tar.gz nemesis_repo.db
+mv -v nemesis_repo.files.tar.gz nemesis_repo.files
+cd ..
+echo "####################################"
+echo "Repo Updated!!"
+echo "####################################"
